8878e1bee92a151038968b11ff96ddaa89138840
jf7890 <nguyenvannhan601@gmail.com>
2025-11-07 09:42:43 +0700

    update IP and network type

--- setup.sh
+++ setup.sh
@@ -2,13 +2,6 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Create a directory for logs
-mkdir -p /var/log/modsecurity
-
-# Set permissions
-chown -R www-data:www-data /var/log/modsecurity
-chmod -R 775 /var/log/modsecurity
-
 # Tên network
 NETWORK_NAME="capstone-network"
 
